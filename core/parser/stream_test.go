912178bf52c2075f5e11ac61d0790018b78880e0
JORGE ARTURO MARTINEZ SANCHEZ <arturo.martinezs@sat.gob.mx>
2025-03-09 16:27:21 -0600

    Added a new test for stream test

--- core/parser/stream_test.go
+++ core/parser/stream_test.go
@@ -7,6 +7,66 @@ import (
 	"testing"
 )
 
+func TestReadUntilFound_Should_Find_Instance(t *testing.T) {
+	mockReader := NewMockReader([]byte{'h', 'o', 'l', 'a'}, 8)
+	stream := Stream{bufio.NewReader(&mockReader)}
+	readBytes, err := stream.ReadUntilFound('l')
+	if err != nil {
+		t.Errorf("Unable to find 'l' inside stream!: %e", err)
+	}
+	if len(readBytes) != 3 {
+		t.Errorf("readBytes len is not 3! %v", len(readBytes))
+	}
+}
+
+func TestReadUntilFound_Should_Read_Whole_Array_When_Not_Found(t *testing.T) {
+	mockReader := NewMockReader([]byte{'h', 'o', 'l', 'a'}, 4)
+	stream := Stream{bufio.NewReader(&mockReader)}
+	bytesRead, err := stream.ReadUntilFound('x')
+	if err != io.EOF {
+		t.Errorf("Unexpected error occurred! %e", err)
+	}
+	if len(bytesRead) != 4 {
+		t.Errorf("len for bytesRead is not 4! %v", len(bytesRead))
+	}
+}
+
+func TestReadUntilSliceFound_Should_Find_Whole_Slice_When_Present(t *testing.T) {
+	mockReader := NewMockReader([]byte{'h', 'o', 'l', 'a'}, 6)
+	stream := Stream{bufio.NewReader(&mockReader)}
+	bytesRead, err := stream.ReadUntilSliceFound([]byte{'l', 'a'})
+	if err != nil {
+		t.Errorf("Unexpected error occurred! %e", err)
+	}
+	if len(bytesRead) != 2 {
+		t.Errorf("len for bytesRead is not 2! %v", len(bytesRead))
+	}
+	mockBytes := []byte{'h', 'o'}
+	for i := range bytesRead {
+		if mockBytes[i] != bytesRead[i] {
+			t.Errorf("bytesRead is not '[h,o]'! %v", bytesRead)
+		}
+	}
+}
+
+func TestReadUntilSliceFound_Should_Return_Error_When_First_Element_Found_But_The_Rest_Is_Different(t *testing.T) {
+	mockReader := NewMockReader([]byte{'h', 'o', 'l', 'a'}, 6)
+	stream := Stream{bufio.NewReader(&mockReader)}
+	bytesRead, err := stream.ReadUntilSliceFound([]byte{'l', 'x'})
+	if err == nil {
+		t.Errorf("Error did not occur!")
+	}
+	if len(bytesRead) != 2 {
+		t.Errorf("len for bytesRead is not 2! %v", len(bytesRead))
+	}
+	mockBytes := []byte{'h', 'o'}
+	for i := range bytesRead {
+		if mockBytes[i] != bytesRead[i] {
+			t.Errorf("bytesRead is not '[h,o]'! %v", bytesRead)
+		}
+	}
+}
+
 func TestReadNBytes_Should_Read_400_Bytes_When_Asked(t *testing.T) {
 	mockReader := NewMockReader([]byte{'h', 'o', 'l', 'a'}, 1000)
 	stream := Stream{bufio.NewReader(&mockReader)}
@@ -28,30 +88,6 @@ func TestReadNBytes_Should_Return_Error_When_EOF(t *testing.T) {
 	}
 	if err == nil || err != io.ErrUnexpectedEOF {
 		t.Errorf("Unable to create an error when exceeding capacity!: %e", err)
-	}
-}
-
-func TestReadUntilFound_Should_Find_Instance(t *testing.T) {
-	mockReader := NewMockReader([]byte{'h', 'o', 'l', 'a'}, 8)
-	stream := Stream{bufio.NewReader(&mockReader)}
-	readBytes, err := stream.ReadUntilFound('l')
-	if err != nil {
-		t.Errorf("Unable to find 'l' inside stream!: %e", err)
-	}
-	if len(readBytes) != 3 {
-		t.Errorf("readBytes len is not 3! %v", len(readBytes))
-	}
-}
-
-func TestReadUntilFound_Should_Read_Whole_Array_When_Not_Found(t *testing.T) {
-	mockReader := NewMockReader([]byte{'h', 'o', 'l', 'a'}, 4)
-	stream := Stream{bufio.NewReader(&mockReader)}
-	bytesRead, err := stream.ReadUntilFound('x')
-	if err != io.EOF {
-		t.Errorf("Unexpected error occurred! %e", err)
-	}
-	if len(bytesRead) != 4 {
-		t.Errorf("Unexpected len for bytes read is not 4! %v", len(bytesRead))
 	}
 }
 
