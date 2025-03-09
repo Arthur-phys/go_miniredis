60819ec6498fc01221c4efecd5996883cd27efbf
JORGE ARTURO MARTINEZ SANCHEZ <arturo.martinezs@sat.gob.mx>
2025-03-09 14:34:51 -0600

    Added tests to resp_types file

---
+++ core/parser/resp_types_test.go
@@ -0,0 +1,51 @@
+package parser
+
+import (
+	"fmt"
+	e "miniredis/error"
+	"testing"
+)
+
+func TestBlobStringToRESP_Should_Return_Expected_Formatted_Bytes(t *testing.T) {
+	sampleStr := "a samplë"
+	byteString := BlobStringToRESP(sampleStr)
+	arr := []byte{'$', '9', '\r', '\n', 'a', ' ', 's', 'a', 'm', 'p', 'l', 195, 171, '\r', '\n'}
+	for i := range byteString {
+		if byteString[i] != arr[i] {
+			t.Errorf("Bytes did not match! %v != %v", byteString[i], arr[i])
+		}
+	}
+}
+
+func TestIntToRESP_Should_Return_Expected_Formatted_Bytes(t *testing.T) {
+	sampleInt := -43
+	byteString := IntToRESP(sampleInt)
+	arr := []byte{':', '-', '4', '3', '\r', '\n'}
+	for i := range byteString {
+		if byteString[i] != arr[i] {
+			t.Errorf("Bytes did not match! %v != %v", byteString[i], arr[i])
+		}
+	}
+}
+
+func TestNullToRESP_Should_Return_Expected_Formatted_Bytes(t *testing.T) {
+	byteString := NullToRESP()
+	arr := []byte{'_', '\r', '\n'}
+	for i := range byteString {
+		if byteString[i] != arr[i] {
+			t.Errorf("Bytes did not match! %v != %v", byteString[i], arr[i])
+		}
+	}
+}
+
+func TestErrToRESP_Should_Return_Expected_Formatted_Bytes(t *testing.T) {
+	sampleErr := e.Error{Content: "HI", Code: 22, From: nil}
+	byteString := ErrToRESP(sampleErr)
+	arr := []byte{'-'}
+	arr = fmt.Appendf(arr, "[MiniRedisError-22] HI\n\r\n")
+	for i := range byteString {
+		if byteString[i] != arr[i] {
+			t.Errorf("Bytes did not match! %v != %v", byteString[i], arr[i])
+		}
+	}
+}
