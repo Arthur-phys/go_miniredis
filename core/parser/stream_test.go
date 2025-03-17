fb7cdf81d7fa2926d605f9650b5ef168a786ec2d
JORGE ARTURO MARTINEZ SANCHEZ <arturo.martinezs@sat.gob.mx>
2025-03-17 15:35:51 -0600

    Added mock connection and first test

--- core/parser/stream_test.go
+++ core/parser/stream_test.go
@@ -8,7 +8,7 @@ import (
 )
 
 func TestReadUntilFound_Should_Find_Instance(t *testing.T) {
-	mockReader := NewMockReader([]byte{'h', 'o', 'l', 'a'}, 8)
+	mockReader := NewmockReader([]byte{'h', 'o', 'l', 'a'}, 8)
 	stream := Stream{bufio.NewReader(&mockReader)}
 	readBytes, err := stream.ReadUntilFound('l')
 	if err != nil {
@@ -20,7 +20,7 @@ func TestReadUntilFound_Should_Find_Instance(t *testing.T) {
 }
 
 func TestReadUntilFound_Should_Read_Whole_Array_When_Not_Found(t *testing.T) {
-	mockReader := NewMockReader([]byte{'h', 'o', 'l', 'a'}, 4)
+	mockReader := NewmockReader([]byte{'h', 'o', 'l', 'a'}, 4)
 	stream := Stream{bufio.NewReader(&mockReader)}
 	bytesRead, err := stream.ReadUntilFound('x')
 	if err != io.EOF {
@@ -32,7 +32,7 @@ func TestReadUntilFound_Should_Read_Whole_Array_When_Not_Found(t *testing.T) {
 }
 
 func TestReadUntilSliceFound_Should_Find_Whole_Slice_When_Present(t *testing.T) {
-	mockReader := NewMockReader([]byte{'h', 'o', 'l', 'a'}, 6)
+	mockReader := NewmockReader([]byte{'h', 'o', 'l', 'a'}, 6)
 	stream := Stream{bufio.NewReader(&mockReader)}
 	bytesRead, err := stream.ReadUntilSliceFound([]byte{'l', 'a'})
 	if err != nil {
@@ -50,7 +50,7 @@ func TestReadUntilSliceFound_Should_Find_Whole_Slice_When_Present(t *testing.T) 
 }
 
 func TestReadUntilSliceFound_Should_Return_Whole_Slice_From_Reader_When_Slice_Looked_For_Is_Not_Present(t *testing.T) {
-	mockReader := NewMockReader([]byte{'h', 'o', 'l', 'a'}, 8)
+	mockReader := NewmockReader([]byte{'h', 'o', 'l', 'a'}, 8)
 	stream := Stream{bufio.NewReader(&mockReader)}
 	bytesRead, err := stream.ReadUntilSliceFound([]byte{'l', 'x'})
 	if err != io.EOF {
@@ -68,7 +68,7 @@ func TestReadUntilSliceFound_Should_Return_Whole_Slice_From_Reader_When_Slice_Lo
 }
 
 func TestReadNBytes_Should_Read_400_Bytes_When_Asked(t *testing.T) {
-	mockReader := NewMockReader([]byte{'h', 'o', 'l', 'a'}, 1000)
+	mockReader := NewmockReader([]byte{'h', 'o', 'l', 'a'}, 1000)
 	stream := Stream{bufio.NewReader(&mockReader)}
 	readBytes, m, err := stream.ReadNBytes(400)
 	if err != nil {
@@ -80,7 +80,7 @@ func TestReadNBytes_Should_Read_400_Bytes_When_Asked(t *testing.T) {
 }
 
 func TestReadNBytes_Should_Return_Error_When_EOF(t *testing.T) {
-	mockReader := NewMockReader([]byte{'h', 'o', 'l', 'a'}, 3)
+	mockReader := NewmockReader([]byte{'h', 'o', 'l', 'a'}, 3)
 	stream := Stream{bufio.NewReader(&mockReader)}
 	readBytes, m, err := stream.ReadNBytes(400)
 	if m != 3 {
@@ -92,7 +92,7 @@ func TestReadNBytes_Should_Return_Error_When_EOF(t *testing.T) {
 }
 
 func TestSkip_Should_Skip_N_Bytes_When_Asked(t *testing.T) {
-	mockReader := NewMockReader([]byte{'h', 'o', 'l', 'a'}, 80)
+	mockReader := NewmockReader([]byte{'h', 'o', 'l', 'a'}, 80)
 	stream := Stream{bufio.NewReader(&mockReader)}
 	n, err := stream.Skip(10)
 	if err != nil {
@@ -104,7 +104,7 @@ func TestSkip_Should_Skip_N_Bytes_When_Asked(t *testing.T) {
 }
 
 func TestSkip_Should_Return_Error_When_N_Bigger_Than_Reader_Size(t *testing.T) {
-	mockReader := NewMockReader([]byte{'h', 'o', 'l', 'a'}, 80)
+	mockReader := NewmockReader([]byte{'h', 'o', 'l', 'a'}, 80)
 	stream := Stream{bufio.NewReader(&mockReader)}
 	n, err := stream.Skip(81)
 	if err == nil {
@@ -115,30 +115,30 @@ func TestSkip_Should_Return_Error_When_N_Bigger_Than_Reader_Size(t *testing.T) {
 	}
 }
 
-type MockReader struct {
+type mockReader struct {
 	bytesArr         []byte
 	currentBytesRead int
 	limitBytes       int
-	read             func(b []byte, mc *MockReader) (int, error)
+	read             func(b []byte, mc *mockReader) (int, error)
 }
 
-func NewMockReader(bytes []byte, limitBytes int) MockReader {
-	read := func(b []byte, mc *MockReader) (int, error) {
+func NewmockReader(bytes []byte, limitBytes int) mockReader {
+	read := func(b []byte, mc *mockReader) (int, error) {
 		n, i := int(math.Min(float64(len(b)), float64(len(mc.bytesArr)))), 0
 		for i < n {
 			b[i] = mc.bytesArr[i]
 			i++
 			mc.currentBytesRead++
 			if mc.currentBytesRead >= mc.limitBytes {
-				defer func() { mc.read = func(b []byte, mc *MockReader) (int, error) { return 0, io.EOF } }()
+				defer func() { mc.read = func(b []byte, mc *mockReader) (int, error) { return 0, io.EOF } }()
 				return i, io.EOF
 			}
 		}
 		return n, nil
 	}
-	return MockReader{bytes, 0, limitBytes, read}
+	return mockReader{bytes, 0, limitBytes, read}
 }
 
-func (mc *MockReader) Read(b []byte) (int, error) {
+func (mc *mockReader) Read(b []byte) (int, error) {
 	return mc.read(b, mc)
 }
--- server/worker_test.go
+++ server/worker_test.go
@@ -1,9 +1,105 @@
 package server
 
 import (
+	"fmt"
+	"io"
+	"log/slog"
+	"math"
+	"miniredis/core/caches"
+	"miniredis/core/parser"
+	"net"
+	"os"
 	"testing"
+	"time"
 )
 
 func TestWorkerhandleConnedction_Should_Return_Message_To_Client_When_Sent_A_Single_One(t *testing.T) {
-	// workerInstantiator = NewWorkerInstantiator()
+
+	logger := slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	slog.SetDefault(logger)
+
+	workerInstantiator := NewWorkerInstantiator(parser.NewRESPParser)
+	cacheStore := caches.NewSimpleCacheStore()
+	channel := make(chan net.Conn)
+	newWorker := workerInstantiator(cacheStore, channel, 1)
+
+	var genericConn net.Conn
+	newConnection := newMockConnection(fmt.Appendf([]byte{}, "*3\r\n$3\r\nSET\r\n$1\r\nB\r\n$7\r\ncrayoli\r\n"), 33)
+	genericConn = &newConnection
+	newWorker.handleConnection(&genericConn)
+	if string(newConnection.writeArr) != "_\r\n" {
+		t.Errorf("Unexpected message received! %v", newConnection.writeArr)
+	}
+}
+
+type mockConnection struct {
+	bytesArr         []byte
+	writeArr         []byte
+	currentBytesRead int
+	limitBytes       int
+	read             func(b []byte, mc *mockConnection) (int, error)
+	write            func(b []byte, mc *mockConnection) (int, error)
+}
+
+func newMockConnection(bytes []byte, limitBytes int) mockConnection {
+	read := func(b []byte, mc *mockConnection) (int, error) {
+		n, i := int(math.Min(float64(len(b)), float64(len(mc.bytesArr)))), 0
+		for i < n {
+			b[i] = mc.bytesArr[i]
+			i++
+			mc.currentBytesRead++
+			if mc.currentBytesRead >= mc.limitBytes {
+				return i, io.EOF
+			}
+		}
+		return n, nil
+	}
+	write := func(b []byte, mc *mockConnection) (int, error) {
+		mc.writeArr = append(mc.writeArr, b...)
+		return len(mc.writeArr), nil
+	}
+	return mockConnection{bytes, []byte{}, 0, limitBytes, read, write}
+}
+
+func (mc *mockConnection) Read(b []byte) (int, error) {
+	return mc.read(b, mc)
+}
+
+func (mc *mockConnection) Write(b []byte) (int, error) {
+	return mc.write(b, mc)
+}
+
+func (mc *mockConnection) Close() error {
+	mc.read = func(b []byte, mc *mockConnection) (int, error) { return 0, io.EOF }
+	mc.write = func(b []byte, mc *mockConnection) (int, error) { return 0, io.EOF }
+	return nil
+}
+
+func (mc *mockConnection) SetDeadline(t time.Time) error {
+	return nil
+}
+
+func (mc *mockConnection) SetWriteDeadline(t time.Time) error {
+	return nil
+}
+func (mc *mockConnection) SetReadDeadline(t time.Time) error {
+	return nil
+}
+
+func (mc *mockConnection) LocalAddr() net.Addr {
+	return mockAddr{}
+}
+
+func (mc *mockConnection) RemoteAddr() net.Addr {
+	return mockAddr{}
+}
+
+type mockAddr struct{}
+
+func (ma mockAddr) Network() string {
+	return "no network"
+}
+
+func (ma mockAddr) String() string {
+	return "test"
 }
