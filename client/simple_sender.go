c474f74438a5b87dc0c2c989db0722266260effa
JORGE ARTURO MARTINEZ SANCHEZ <arturo.martinezs@sat.gob.mx>
2025-03-31 21:34:34 -0600

    Added one more function before going to sleep

--- client/simple_sender.go
+++ client/simple_sender.go
@@ -9,21 +9,26 @@ import (
 type SimpleSender struct{}
 
 func (ss *SimpleSender) get(key string) ([]byte, func(s *rt.Stream) (string, e.Error)) {
-	return fmt.Appendf([]byte{}, fmt.Sprintf(`*2\r\n$3\r\nGET\r\n$%d\r\n%v\r\n`, len(key), key)),
+	return fmt.Appendf([]byte{}, fmt.Sprintf("*2\r\n$3\r\nGET\r\n$%d\r\n%v\r\n", len(key), key)),
 		rt.BlobStringFromBytes
 
 }
 
 func (ss *SimpleSender) set(key string, value string) ([]byte, func(s *rt.Stream) e.Error) {
-	return fmt.Appendf([]byte{}, fmt.Sprintf(`*3\r\n$3\r\nSET\r\n$%d\r\n%v\r\n$%d\r\n%v\r\n`, len(key), key, len(value), value)), rt.ErrorFromBytes
+	return fmt.Appendf([]byte{}, fmt.Sprintf("*3\r\n$3\r\nSET\r\n$%d\r\n%v\r\n$%d\r\n%v\r\n", len(key), key, len(value), value)), rt.ErrorFromBytes
 }
 
-func (ss *SimpleSender) rPush(key string, args ...string) ([]byte, func([]byte) error) {
-	// finalStr := ""
+func (ss *SimpleSender) rPush(key string, args ...string) ([]byte, func(s *rt.Stream) e.Error) {
+	finalBytes := fmt.Appendf([]byte{}, fmt.Sprintf("*%d\r\n$5\r\nRPUSH", len(args)+1))
+	for i := range args {
+		finalBytes = fmt.Appendf(finalBytes, fmt.Sprintf("\r\n%d\r\n%v", len(args[i]), args[i]))
+	}
+	finalBytes = fmt.Appendf(finalBytes, "\r\n")
+	return finalBytes, rt.ErrorFromBytes
 }
 
-func (ss *SimpleSender) rPop(key string) ([]byte, func([]byte) (string, error))              {}
-func (ss *SimpleSender) lLen(key string) ([]byte, func([]byte) (uint, error))                {}
-func (ss *SimpleSender) lPop(key string) ([]byte, func([]byte) (string, error))              {}
-func (ss *SimpleSender) lPush(key string, args ...string) ([]byte, func([]byte) error)       {}
-func (ss *SimpleSender) lIndex(key string, index int) ([]byte, func([]byte) (string, error)) {}
+// func (ss *SimpleSender) rPop(key string) ([]byte, func([]byte) (string, error))              {}
+// func (ss *SimpleSender) lLen(key string) ([]byte, func([]byte) (uint, error))                {}
+// func (ss *SimpleSender) lPop(key string) ([]byte, func([]byte) (string, error))              {}
+// func (ss *SimpleSender) lPush(key string, args ...string) ([]byte, func([]byte) error)       {}
+// func (ss *SimpleSender) lIndex(key string, index int) ([]byte, func([]byte) (string, error)) {}
