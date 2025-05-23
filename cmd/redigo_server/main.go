c3ff9b950277341c877536419932da4faf98643d
JORGE ARTURO MARTINEZ SANCHEZ <arturo.jms.prog@gmail.com>
2025-05-22 18:09:32 -0600

    Shortened flags explanations

--- cmd/redigo_server/main.go
+++ cmd/redigo_server/main.go
@@ -6,14 +6,12 @@ import (
 	"flag"
 	"fmt"
 	"regexp"
-	"strings"
 
 	"github.com/Arthur-phys/redigo/pkg/server"
 )
 
 var ipAddress string
 var port uint
-var strCache string
 var messageSizeLimit int
 var workerAmount uint64
 var keepAlive int64
@@ -22,11 +20,10 @@ var shutdownTolerance int64
 func init() {
 	flag.StringVar(&ipAddress, "ip", "127.0.0.1", "Binding IP address for server.")
 	flag.UintVar(&port, "port", 6543, "Binding Port for server.")
-	flag.StringVar(&strCache, "cache_type", "Cache", "Type of cache to use. Available caches are:\n - 'Cache': Has a simple interface and sub-optimal alogrithms. It mostly served as a placeholder for tests in this library. Nonetheless, it can be useful if you plan to use redigo for a small app with not too much traffic\n - 'Cache': A cache made to be used by a large application that allows multiple goroutines to access the dictionary at the same time granted the key looked for is in different shards.")
-	flag.IntVar(&messageSizeLimit, "message_size", 10240, "Limit in size (bytes) for a single message delivered to the server. This size does not correspond with the size of a given TCP segment, rather it is the sum of the size of the content of one or more segments.\nWhenever you send a command to the server which it is unable to be parsed in a single call to read (the socket), every extra call will be summed to the total size read. If this limit is surpassed, the message will be discarded and an error will be returned.\nThis also applies to multi-command messages in which one or more messages are able to be parsed but another is incomplete and requires another call to read.")
-	flag.Uint64Var(&workerAmount, "worker_amount", 10, "Number of workers to initialize. Every worker will be able to attend a connection independently until this connection is closed or the keepAlive parameter is exceeded.")
-	flag.Int64Var(&keepAlive, "keep_alive", 15, "Time (in seconds) to keep a connection open if no message is received. Connections will be closed if no message is received during this period. When this happens, the worker will close the connection and start serving another one.")
-	flag.Int64Var(&shutdownTolerance, "shutdown", 15, "Time (in seconds) given to workers when gracefully shutting down the server")
+	flag.IntVar(&messageSizeLimit, "message_size", 10240, "Limit in size (bytes) for a single message delivered to the server.")
+	flag.Uint64Var(&workerAmount, "worker_amount", 10, "Number of workers to initialize.")
+	flag.Int64Var(&keepAlive, "keep_alive", 15, "Time (in seconds) to keep a connection open if no message is received.")
+	flag.Int64Var(&shutdownTolerance, "shutdown", 15, "Time (in seconds) given to workers when gracefully shutting down the server.")
 }
 
 func main() {
@@ -40,7 +37,6 @@ func main() {
 		fmt.Printf("Unable to convert given port number (%d) to the corresponding range 0 - 65535\n", port)
 		return
 	}
-	strCache = strings.ToLower(strCache)
 
 	serverConfig := server.Configuration{
 		IpAddress:         ipAddress,
